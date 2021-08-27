15d68ab2da501d6e053008458ad89366f071c9e9
magmaus3 <magmaus3@disroot.org>
2021-08-27 16:00:10 +0200

    Remove <author> from url

--- example_package.sh
+++ example_package.sh
@@ -4,15 +4,16 @@ echo "Generating example package..."
 
 mkdir -p packages/author/package/releases/0/
 
-echo "If you are seeing this file, everything works correctly" > packages/author/package/releases/0/message.txt
+echo "If you are seeing this file, everything works correctly" > packages/package/releases/0/message.txt
 echo "{
         "author": "author",
         "name": "package-name",
         "title": "Package Title",
         "short_description": "example package",
         "release": 0
-}" > packages/author/package/meta.json
+}" > packages/package/meta.json
 
-zip packages/author/package/releases/0/pack.zip packages/author/package/releases/0/message.txt
+cd packages/package/releases/0/
+zip pack.zip message.txt
 
 echo "Done!"
